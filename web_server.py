26f9b9cc4b4825d27bfc5d26229bb850fc7ea52d
treshshina-git <treshshina@gmail.com>
2026-06-13 23:55:33 +0200

    Update web_server.py

--- web_server.py
+++ web_server.py
@@ -1,4 +1,5 @@
 import asyncio
+import os
 from aiogram import Bot, Dispatcher, F
 from aiogram.types import InlineQuery, InlineQueryResultArticle, InputTextMessageContent
 from aiogram.utils.keyboard import InlineKeyboardBuilder
