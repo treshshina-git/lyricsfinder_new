732519eb3e5128391bb41c24ccc13e16172902b7
treshshina-git <treshshina@gmail.com>
2026-06-14 19:45:08 +0200

    g

--- old_new/lrclib_api.py
+++
@@ -1,9 +0,0 @@
-
-import aiohttp
-async def get_lyrics(artist,title):
-    async with aiohttp.ClientSession() as s:
-        async with s.get("https://lrclib.net/api/get",
-            params={"artist_name":artist,"track_name":title}) as r:
-            if r.status!=200: return None
-            d=await r.json()
-            return d.get("plainLyrics") or d.get("syncedLyrics")
