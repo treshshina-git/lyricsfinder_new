88f89f77ba3d941d1072b707a7d7569ac53d51ae
treshshina-git <treshshina@gmail.com>
2026-06-14 07:43:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -52,15 +52,9 @@ async def inline_query(query):
     await bot.answer_inline_query(query.id, results, cache_time=30, is_personal=True)
 
 
-def main_loop():
-    bot.infinity_polling()
-    while 1:
-        time.sleep(10)
-
-
 if __name__ == "__main__":
-    try:
-        main_loop()
-    except KeyboardInterrupt:
-        print("\nExiting by user request.\n")
-        sys.exit(0)
+    asyncio.run(
+        bot.infinity_polling(
+            skip_pending=True
+        )
+    )
